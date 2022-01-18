aefd329293d4505c7ad33cb1be45607010df4ac9
Natedog6192 <81579413+Natedog6192@users.noreply.github.com>
2022-01-17 21:36:16 -0600

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var e4 = document.getElementById("e4");
+	e4.addEventListener("click", function() {
+		window.open("https://lichess.org/study/lQO4LLPh", "_blank");
+	});
+	var d5 = document.getElementById("d5");
+	d5.addEventListener("click", function() {
+		window.open("https://lichess.org/study/tJq9ZAYX", "_blank");
+	});
+	var caro = document.getElementById("Caro");
+	caro.addEventListener("click", function() {
+		window.open("https://lichess.org/study/w3WnOCtF", "_blank");
+	});
+	var e5 = document.getElementById("e5");
+	e5.addEventListener("click", function() {
+		window.open("https://lichess.org/study/VpWDItJO/gBoOlqT8", "_blank");
+	});
+
+});
